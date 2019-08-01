8a02ea203d5c62b0954ad832d21ad2f6cf247104
SkyLined <github@skylined.nl>
2019-08-01 13:44:34 +0200

    Add Tests

---
+++ Tests.cmd
@@ -0,0 +1,43 @@
+@ECHO OFF
+SETLOCAL
+
+REM If the user wants to run a full test-suite, new reports will be generated so the old ones must be deleted so as to
+REM not leave old reports intermingled with the new ones.
+IF "%~1" == "--full" (
+  REM If you can add the x86 and x64 binaries of python to the path, or add links to the local folder, tests will be run
+  REM in both
+  WHERE PYTHON_X86 >nul 2>&1
+  IF NOT ERRORLEVEL 0 (
+    ECHO - PYTHON_X86 was not found; not testing both x86 and x64 ISAs.
+  ) ELSE (
+    WHERE PYTHON_X64 >nul 2>&1
+    IF NOT ERRORLEVEL 0 (
+      ECHO - PYTHON_X64 was not found; not testing both x86 and x64 ISAs.
+    ) ELSE (
+      GOTO :TEST_BOTH_ISAS
+    )
+  )
+)
+
+WHERE PYTHON 2>&1 >nul
+IF ERRORLEVEL 1 (
+  ECHO - PYTHON was not found!
+  EXIT /B 1
+)
+
+CALL PYTHON "%~dp0\%~n0\%~n0.py" %*
+IF ERRORLEVEL 1 GOTO :ERROR
+ENDLOCAL & EXIT /B 0
+
+:TEST_BOTH_ISAS
+  ECHO * Running tests in x86 build of Python...
+  CALL PYTHON_X86 "%~dp0\%~n0.py" %*
+  IF ERRORLEVEL 1 GOTO :ERROR
+  ECHO * Running tests in x64 build of Python...
+  CALL PYTHON_X64 "%~dp0\%~n0.py" %*
+  IF ERRORLEVEL 1 GOTO :ERROR
+  ENDLOCAL & EXIT /B 0
+
+:ERROR
+  ECHO - Error %ERRORLEVEL%!
+  ENDLOCAL & EXIT /B %ERRORLEVEL%
---
+++ Tests/Tests.py
@@ -0,0 +1,50 @@
+import os, re, sys, subprocess, threading, time;
+sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
+sMainFolderPath = os.path.dirname(sTestsFolderPath);
+sParentFolderPath = os.path.dirname(sMainFolderPath);
+sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
+asOriginalSysPath = sys.path[:];
+sys.path = [sMainFolderPath, sParentFolderPath, sModulesFolderPath] + sys.path;
+# Save the list of names of loaded modules:
+asOriginalModuleNames = sys.modules.keys();
+
+from mWindowsSDK import *;
+
+# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
+# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
+# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
+for sModuleName in sys.modules.keys():
+  assert (
+    sModuleName in asOriginalModuleNames # This was loaded before cBugId was loaded
+    or sModuleName.lstrip("_").split(".", 1)[0] in [
+      "mWindowsSDK", # This was loaded as part of the mWindowsAPI package
+      # These built-in modules are loaded by mWindowsSDK:
+      "ctypes", "struct", "platform", "string", "strop",
+    ]
+  ), \
+      "Module %s was unexpectedly loaded outside of the mWindowsAPI package!" % sModuleName;
+# Restore the search path
+sys.path = asOriginalSysPath;
+
+oKernel32 = cDLL("kernel32.dll", {
+  "GetLastError": {
+    "xReturnType": DWORD,
+  },
+  "GetWindowsDirectoryW": {
+    "xReturnType": UINT,
+    "txArgumentTypes": (LPWSTR, UINT),
+  },
+});
+
+uBufferSize = MAX_PATH;
+oBuffer = foCreateBuffer(uBufferSize, bUnicode = True);
+ouResult = oKernel32.GetWindowsDirectoryW(
+  oBuffer.foCreatePointer(LPWSTR), # lpBuffer
+  uBufferSize, # uSize
+);
+assert ouResult.value != 0, \
+    "oKernel32.GetWindowsDirectoryW(lpBuffer=0x%X, uSize=0x%X) => 0x%X" % \
+    (oBuffer.fuGetAddress(), uBufferSize, oKernel32.GetLastError().value);
+assert ouResult.value <= uBufferSize, \
+    "Path is larger than MAX_PATH !?";
+print "Windows directory = %s" % oBuffer.fsGetString();
