01b49b03b34e8bb7fb9f51f005f66c4838ce7d92
SkyLined <github@skylined.nl>
2019-08-02 10:22:33 +0200

    Fix error handling success :(

--- HRESULT_FROM_WIN32.py
+++ HRESULT_FROM_WIN32.py
@@ -1,6 +1,7 @@
 from .mPrimitiveTypes import HRESULT;
 
 def HRESULT_FROM_WIN32(uWin32):
+  if uWin32 == 0: return HRESULT(0);
   assert uWin32 & 0xFFFF0000 == 0, \
       "Invalid WIN32 value 0x%X" % uWin32;
   return HRESULT(0x80070000 + uWin32);
