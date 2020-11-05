7739628581d7a7ee3bfc46d2166092c0914ac8db
SkyLined <github@skylined.nl>
2020-11-05 12:56:48 +0100

    Improve cCharacterType.__repr__ value

--- cCharacterType.py
+++ cCharacterType.py
@@ -3,10 +3,10 @@ from cPrimitiveType import cPrimitiveType;
 class cCharacterType(cPrimitiveType):
   def __repr__(oSelf):
     sChar = chr(oSelf.value) if oSelf.value >= 0x20 and oSelf.value < 0x7F else None;
-    return "<char %s:%d(0x%X%s) @ 0x%X>" % (
+    return "<char %s:%d(%s%s%s) @ 0x%X>" % (
       oSelf.__class__.__name__,
       oSelf.fuGetSize(),
-      oSelf.value,
+      "%%0%dX" % (2 * oSelf.fuGetSize()) % oSelf.value,
       "" if sChar is None else ", '%s%s'" % ("\\" if sChar in "'\\" else "", sChar),
       oSelf.fuGetAddress(),
     );
