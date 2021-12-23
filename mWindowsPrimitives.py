03957e748a2129422b6f6b574cfd0e52b1db9276
SkyLined <github@skylined.nl>
2021-12-23 16:39:10 +0100

    Allow types to be based on other types.

--- mWindowsPrimitives.py
+++ mWindowsPrimitives.py
@@ -15,7 +15,7 @@ from .mPointerClasses import \
     cVoidPointer32, cVoidPointer64, cVoidPointer;
 from .uProcessBits import uProcessBits;
 
-di0BaseType_by_sName = {
+dx0BaseType_by_sName = {
   #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
   "ASTR":     cCharacterA, # Used to create LPASTR etc.
   #BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
@@ -53,7 +53,7 @@ di0BaseType_by_sName = {
   "HLOCAL":   cVoidPointer,
   "HMENU":    cVoidPointer,
   "HMETAFILE": cVoidPointer,
-  "HMODULE":  cVoidPointer,
+  "HMODULE":  "HANDLE", # Sub-type
   "HMONITOR": cVoidPointer,
   "HPALETTE": cVoidPointer,
   "HPEN":     cVoidPointer,
@@ -63,7 +63,7 @@ di0BaseType_by_sName = {
   "HSTR":     cVoidPointer,
   "HTASK":    cVoidPointer,
   "HWINSTA":  cVoidPointer,
-  "HWND":     cVoidPointer,
+  "HWND":     "HANDLE", # Sub-type
   #IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
   "INT":      cSignedInteger32,
   "INT_PTR":  cSignedInteger,
@@ -120,18 +120,7 @@ def fExport(sName, xValue):
   globals()[sName] = xValue; # Make it available in the context of this file
   __all__.append(sName); # Make it available as an export from this module.
 
-for (sName, i0BaseType) in di0BaseType_by_sName.items():
-  if i0BaseType: # We do not export "VOID" but it is in the list to export "PVOID"
-    cType = type(sName, (i0BaseType,), {"sName": sName});
-    fExport(sName, cType);
-    cPointerType = cType.fcCreatePointer();
-    cTargetPointer32 = cType.fcCreatePointer32();
-    cTargetPointer64 = cType.fcCreatePointer64();
-  else:
-    cPointerType = cVoidPointer;
-    cTargetPointer32 = cVoidPointer32;
-    cTargetPointer64 = cVoidPointer64;
-    
+def fExportPointers(sName, c0BaseType, cPointerType, cTargetPointer32, cTargetPointer64):
   fExport("P%s" % sName,   cPointerType);
   fExport("LP%s" % sName,  cPointerType);
   fExport("PP%s" % sName,  cPointerType.fcCreatePointer());
@@ -139,6 +128,43 @@ for (sName, i0BaseType) in di0BaseType_by_sName.items():
   fExport("P32%s" % sName, cTargetPointer32);
   fExport("P64%s" % sName, cTargetPointer64);
   # For types based on a pointer, we explicitly export 32- and 64-bit versions.
-  if i0BaseType is cVoidPointer:
+  if c0BaseType is cVoidPointer:
     fExport(sName + "32", type(sName + "32", (cVoidPointer32,), {"sName": sName + "32"}));
     fExport(sName + "64", type(sName + "32", (cVoidPointer64,), {"sName": sName + "32"}));
+
+# Some types are based on other types:
+dsBaseTypeName_by_sName = {};
+for (sName, x0BaseType) in dx0BaseType_by_sName.items():
+  if isinstance(x0BaseType, str):
+    dsBaseTypeName_by_sName[sName] = x0BaseType;
+    continue;
+  c0BaseType = x0BaseType;
+  if x0BaseType is None:
+    # The type "VOID" does not exist, but void pointers do (e.g. "PVOID")
+    cPointerType = cVoidPointer;
+    cTargetPointer32 = cVoidPointer32;
+    cTargetPointer64 = cVoidPointer64;
+  else:
+    cType = type(sName, (c0BaseType,), {"sName": sName});
+    fExport(sName, cType);
+    cPointerType = cType.fcCreatePointer();
+    cTargetPointer32 = cType.fcCreatePointer32();
+    cTargetPointer64 = cType.fcCreatePointer64();
+  fExportPointers(sName, c0BaseType, cPointerType, cTargetPointer32, cTargetPointer64);
+
+while dsBaseTypeName_by_sName:
+  dsUnprocessedBaseTypeName_by_sName = {};
+  for (sName, sBaseName) in dsBaseTypeName_by_sName.items():
+    if sBaseName not in globals():
+      dsUnprocessedBaseTypeName_by_sName[sName] = sBaseName;
+      continue;
+    c0BaseType = globals()[sBaseName];
+    cType = type(sName, (c0BaseType,), {"sName": sName});
+    fExport(sName, cType);
+    fExportPointers(sName, c0BaseType, cPointerType, cTargetPointer32, cTargetPointer64);
+  assert len(dsUnprocessedBaseTypeName_by_sName) < len(dsBaseTypeName_by_sName), \
+      "cyclic dependencies in: %s" % ", ".join(
+        "%s=>%s" % (sName, sBaseName)
+        for (sName, sBaseName) in dsUnprocessedBaseTypeName_by_sName.items()
+      );
+  dsBaseTypeName_by_sName = dsUnprocessedBaseTypeName_by_sName;
