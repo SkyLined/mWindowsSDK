9c11b52635e22578a87db2924c983d30ae1f9cea
SkyLined <cojmpjum_github.com@sxd.be>
2022-06-21 09:34:54 +0200

    Add __all__ to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
+asOldGlobals = list(globals().keys()) + ["asOldGlobals", "fInitializeProduct", "mExports"];
+
 from .fInitializeProduct import fInitializeProduct;
 fInitializeProduct();
 
 from .mExports import *;
+
+__all__ = [sName for sName in globals().keys() if sName not in asOldGlobals];
