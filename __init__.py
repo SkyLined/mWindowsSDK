d959e3e3518ac93d8d42d7aa9b973278f1db039d
SkyLined <cojmpjum_github.com@sxd.be>
2022-06-22 14:23:55 +0200

    ~ Updated standard files.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,4 @@
-asOldGlobals = list(globals().keys()) + ["asOldGlobals", "fInitializeProduct", "mExports"];
-
 from .fInitializeProduct import fInitializeProduct;
 fInitializeProduct();
 
 from .mExports import *;
-
-__all__ = [sName for sName in globals().keys() if sName not in asOldGlobals];
